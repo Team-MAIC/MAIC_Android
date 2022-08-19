9ef4ed56f03073dd99f4c78f49a7777f1456652f
hjh1161514 <hjh1161514@naver.com>
2022-08-19 23:57:04 +0900

    [FEAT] UI - ItemActivity

---
+++ app/src/main/java/com/maic/kurlyhack/feature/picking/ItemActivity.kt
@@ -0,0 +1,12 @@
+package com.maic.kurlyhack.feature.picking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maic.kurlyhack.R
+
+class ItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item)
+    }
+}
