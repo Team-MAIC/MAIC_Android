1701af7751049ede58db344f9e295c7693eaf357
hjh1161514 <hjh1161514@naver.com>
2022-08-19 23:56:21 +0900

    [FEAT] UI - MainActivity

--- app/src/main/java/com/maic/kurlyhack/feature/MainActivity.kt
+++ app/src/main/java/com/maic/kurlyhack/feature/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.maic.kurlyhack
+package com.maic.kurlyhack.feature
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.maic.kurlyhack.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
