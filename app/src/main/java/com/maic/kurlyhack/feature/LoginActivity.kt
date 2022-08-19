12c2f6c7f76ae27a7c5aca39adbcedcb124e9c01
hjh1161514 <hjh1161514@naver.com>
2022-08-20 00:53:25 +0900

    [FEAT] Active btn and click btn listener

--- app/src/main/java/com/maic/kurlyhack/feature/LoginActivity.kt
+++ app/src/main/java/com/maic/kurlyhack/feature/LoginActivity.kt
@@ -1,12 +1,35 @@
 package com.maic.kurlyhack.feature
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.maic.kurlyhack.R
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
+import com.maic.kurlyhack.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        activeBtn()
+        clickBtnListener()
+    }
+
+    private fun activeBtn() {
+        with(binding) {
+            etUserNumber.addTextChangedListener {
+                btnMainStart.isEnabled = etUserNumber.text.toString().isNotEmpty()
+            }
+        }
+    }
+
+    private fun clickBtnListener() {
+        binding.btnMainStart.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+            finish()
+        }
     }
 }
