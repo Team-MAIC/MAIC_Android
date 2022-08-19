100998d92c6d45075d9afaa933afb916877430d8
hjh1161514 <hjh1161514@naver.com>
2022-08-19 23:56:43 +0900

    [FEAT] UI - LoginActivity

---
+++ app/src/main/java/com/maic/kurlyhack/feature/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.maic.kurlyhack.feature
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maic.kurlyhack.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
