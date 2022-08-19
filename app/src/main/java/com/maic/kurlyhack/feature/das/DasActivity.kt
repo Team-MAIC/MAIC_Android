4c01d909afb9dacc0a5e22787063e35dce168de8
hjh1161514 <hjh1161514@naver.com>
2022-08-20 00:02:18 +0900

    [FEAT] UI - DasActivity

---
+++ app/src/main/java/com/maic/kurlyhack/feature/das/DasActivity.kt
@@ -0,0 +1,12 @@
+package com.maic.kurlyhack.feature.das
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maic.kurlyhack.R
+
+class DasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_das)
+    }
+}
