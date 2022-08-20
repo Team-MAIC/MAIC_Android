3c5aa56fe48156a9e8ff8711ee818917d28dd224
hjh1161514 <hjh1161514@naver.com>
2022-08-20 15:15:11 +0900

    [FEAT] BarcodeDialog

---
+++ app/src/main/java/com/maic/kurlyhack/feature/picking/BarcodeDialog.kt
@@ -0,0 +1,35 @@
+package com.maic.kurlyhack.feature.picking
+
+import android.app.Dialog
+import android.content.Context
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
+import com.maic.kurlyhack.R
+
+class BarcodeDialog(context: Context) {
+    private val dialog = Dialog(context)
+
+    fun showDialog() {
+        dialog.setContentView(R.layout.dialog_barcode)
+        dialog.window!!.setLayout(
+            WindowManager.LayoutParams.MATCH_PARENT,
+            WindowManager.LayoutParams.WRAP_CONTENT
+        )
+        dialog.setCanceledOnTouchOutside(true)
+        dialog.setCancelable(true)
+        dialog.show()
+
+        val mBtn = dialog.findViewById<Button>(R.id.btn_dialog_confirm)
+        val mEt = dialog.findViewById<EditText>(R.id.et_dialog_input)
+
+        mEt.addTextChangedListener {
+            mBtn.isEnabled = mEt.text.toString().isNotEmpty()
+        }
+
+        mBtn.setOnClickListener {
+            dialog.dismiss()
+        }
+    }
+}
--- app/src/main/java/com/maic/kurlyhack/feature/picking/PickingBarCodeActivity.kt
+++ app/src/main/java/com/maic/kurlyhack/feature/picking/PickingBarCodeActivity.kt
@@ -1,6 +1,7 @@
 package com.maic.kurlyhack.feature.picking
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.zxing.ResultPoint
@@ -9,13 +10,21 @@ import com.journeyapps.barcodescanner.BarcodeResult
 import com.journeyapps.barcodescanner.CaptureManager
 import com.journeyapps.barcodescanner.DecoratedBarcodeView
 import com.maic.kurlyhack.R
+import com.maic.kurlyhack.databinding.ActivityPickingBarcodeBinding
 
 class PickingBarCodeActivity : AppCompatActivity() {
     private lateinit var barcodeScannerView: DecoratedBarcodeView
     private lateinit var capture: CaptureManager
+    private lateinit var binding: ActivityPickingBarcodeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_picking_barcode)
+        binding = ActivityPickingBarcodeBinding.inflate(layoutInflater)
+
+        getData()
+        initDialog()
+
+        setContentView(binding.root)
 
         barcodeScannerView = findViewById(R.id.picking_scanner)
 
@@ -29,6 +38,26 @@ class PickingBarCodeActivity : AppCompatActivity() {
             override fun possibleResultPoints(resultPoints: List<ResultPoint>) {
             }
         })
+    }
+
+    private fun getData() {
+        val infoList = intent.getStringArrayListExtra("pickingInfo")
+        val part = intent.getStringExtra("pickingPart")
+
+        if (infoList != null) {
+            val name = infoList[1].toString()
+            val count = infoList[2].toString()
+            val address = infoList[0].toString()
+            binding.tvPickingBarcodePart.text = "$part $address"
+            binding.tvPickingBarcodeName.text = "$name $count"
+        }
+    }
+
+    private fun initDialog() {
+        binding.btnPickingBarcodeStart.setOnClickListener {
+            val dialog = BarcodeDialog(this)
+            dialog.showDialog()
+        }
     }
 
     override fun onResume() {
